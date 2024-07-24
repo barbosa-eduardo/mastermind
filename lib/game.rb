b93183fb5a95902cdbb1caac2872c8f839028ff1
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 15:48:45 -0300

    Complete

--- lib/game.rb
+++ lib/game.rb
@@ -8,13 +8,13 @@ class Game
 
   def play
     generate_code
-    12.times do
+    12.times do |i|
+      puts "\nRound #{i + 1}"
       guess = input_guess
-      if guess == code
-        end_game('victory')
-        return
-      end
-      puts get_feedback(guess)
+      puts "Your guess is #{guess.join(' ')}"
+      return end_game('victory') if guess == code
+
+      puts "\n#{get_feedback(guess)}"
     end
     end_game('defeat')
   end
@@ -28,17 +28,61 @@ class Game
   end
 
   def input_guess
-    puts 'Type your guess:'
-    gets.chomp.split
+    puts(pegs.each_index.reduce(String.new) { |sum, i| sum + "| #{i + 1} - #{pegs[i]} |" })
+    puts 'The code has 4 colors. To guess type the number or the name of the color.'
+    guess = gets_guess
+    until validate_guess(guess)
+      puts 'Invalid guess! Type the number or color separated by white space. Example: 1 5 2 4'
+      guess = gets_guess
+    end
+    guess
+  end
+
+  def gets_guess
+    guess = gets.chomp.split
+    guess.map do |item|
+      if item.ord.between?(48, 57)
+        i = item.to_i - 1
+        pegs[i]
+      else
+        item
+      end
+    end
+  end
+
+  def validate_guess(guess)
+    return invalid_size unless guess.length == code.length
+
+    guess.each do |item|
+      return invalid_number if item.nil?
+      return invalid_color unless pegs.include?(item)
+    end
+
+    true
+  end
+
+  def invalid_size
+    puts "\nThe code has #{code.length} colors!"
+    false
+  end
+
+  def invalid_number
+    puts "Type numbers between 1 and #{pegs.length}."
+    false
+  end
+
+  def invalid_color
+    puts "\n#{item} is invalid!"
+    false
   end
 
   def end_game(state)
     if state == 'victory'
-      puts 'victory'
+      puts 'Congratulations! The code was discovered.'
     elsif state == 'defeat'
-      puts 'defeat'
+      puts 'You lost! More luck next time.'
     end
-    puts "The code was #{code}"
+    puts "The code was #{code.join(' ')}"
   end
 
   def get_feedback(guess)
@@ -67,7 +111,7 @@ class Game
       end
     end
     guess.compact!
-    "correct: #{correct}\nwrong position #{missed}\n#{guess.length} wrong"
+    "correct: #{correct}\nwrong position: #{missed}\nwrong: #{guess.length}"
   end
 end
 
