273d7dc548f28426cee4c1dff79ff87a3b2c88f8
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 13:01:55 -0300

    refactor Game structure

--- lib/game.rb
+++ lib/game.rb
@@ -6,33 +6,24 @@ class Game
     @pegs = %w[red green blue yellow orange purple]
   end
 
+  def play
+  end
+
   def generate_code
     4.times { code.push(pegs.sample) }
+    p code
+  end
+
+  def input_guess
   end
 
   def generate_feedback(guess)
-    feedback = []
-    guess.each_with_index do |item, index|
-      feedback.push(rate_guess(item, index))
-    end
-    feedback.shuffle
   end
 
   private
 
   attr_accessor :code, :pegs
-
-  def rate_guess(item, position)
-    if item == code[position]
-      return 'correct'
-    elsif code[position..].include?(item)
-      return 'wrong position'
-    end
-
-    'wrong'
-  end
 end
 
 game = Game.new
-game.generate_code
-p game.feedback(%w[green blue orange blue])
+game.play
