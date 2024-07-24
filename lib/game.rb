e0aea77f26784e508c583f1cf9817da711315764
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 14:19:33 -0300

    add get_feedback method

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,7 @@ class Game
       guess = input_guess
       if guess == code
         end_game('victory')
-        break
+        return
       end
       puts get_feedback(guess)
     end
@@ -28,6 +28,8 @@ class Game
   end
 
   def input_guess
+    puts 'Type your guess:'
+    gets.chomp.split
   end
 
   def end_game(state)
@@ -40,6 +42,32 @@ class Game
   end
 
   def get_feedback(guess)
+    code = Array.new(self.code)
+    correct = 0
+    missed = 0
+    guess.each_with_index do |item, index|
+      next unless item == code[index]
+
+      guess[index] = nil
+      code[index] = nil
+      correct += 1
+    end
+
+    guess.compact!
+    code.compact!
+
+    guess.each_index do |i|
+      code.each_index do |j|
+        next unless guess[i] == code[j]
+
+        guess[i] = nil
+        code[j] = nil
+        missed += 1
+        break
+      end
+    end
+    guess.compact!
+    "correct: #{correct}\nwrong position #{missed}\n#{guess.length} wrong"
   end
 end
 
