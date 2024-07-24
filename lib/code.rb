bc55e0859ef5880872f7c07a766f890a38994c2d
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 18:30:49 -0300

    divide game into code_maker ad code_breaker

---
+++ lib/code.rb
@@ -0,0 +1,4 @@
+class Code
+  CODE_LENGTH = 4
+  PEGS = %w[red green blue yellow orange purple].freeze
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-class Game
-  CODE_LENGTH = 4
+require_relative 'code_maker'
 
+class Game
   def initialize
-    @code = []
-    @pegs = %w[red green blue yellow orange purple]
+    @code_maker = CodeMaker.new
   end
 
   def play
-    generate_code
+    code_maker.generate_code
     12.times do |i|
       puts "\nRound #{i + 1}"
       guess = input_guess
       puts "Your guess is #{guess.join(' ')}"
-      return end_game('victory') if guess == code
+      return end_game('victory') if guess == code_maker.code
 
       puts "\n#{get_feedback(guess)}"
     end
@@ -25,59 +24,6 @@ class Game
 
   attr_accessor :code, :pegs
 
-  def generate_code
-    CODE_LENGTH.times { code.push(pegs.sample) }
-  end
-
-  def input_guess
-    puts(pegs.each_index.reduce(String.new) { |sum, i| sum + "| #{i + 1} - #{pegs[i]} |" })
-    puts 'The code has 4 colors. To guess type the number or the name of the color.'
-    guess = gets_guess
-    until validate_guess(guess)
-      puts 'Invalid guess! Type the number or color separated by white space. Example: 1 5 2 4'
-      guess = gets_guess
-    end
-    guess
-  end
-
-  def gets_guess
-    guess = gets.chomp.split
-    guess.map do |item|
-      if item.ord.between?(48, 57)
-        i = item.to_i - 1
-        pegs[i]
-      else
-        item
-      end
-    end
-  end
-
-  def validate_guess(guess)
-    return invalid_size unless guess.length == code.length
-
-    guess.each do |item|
-      return invalid_number if item.nil?
-      return invalid_color unless pegs.include?(item)
-    end
-
-    true
-  end
-
-  def invalid_size
-    puts "\nThe code has #{code.length} colors!"
-    false
-  end
-
-  def invalid_number
-    puts "Type numbers between 1 and #{pegs.length}."
-    false
-  end
-
-  def invalid_color
-    puts "\n#{item} is invalid!"
-    false
-  end
-
   def end_game(state)
     if state == 'victory'
       puts 'Congratulations! The code was discovered.'
@@ -85,34 +31,5 @@ class Game
       puts 'You lost! More luck next time.'
     end
     puts "The code was #{code.join(' ')}"
-  end
-
-  def get_feedback(guess)
-    code = Array.new(self.code)
-    correct = 0
-    missed = 0
-    guess.each_with_index do |item, index|
-      next unless item == code[index]
-
-      guess[index] = nil
-      code[index] = nil
-      correct += 1
-    end
-
-    guess.compact!
-    code.compact!
-
-    guess.each_index do |i|
-      code.each_index do |j|
-        next unless guess[i] == code[j]
-
-        guess[i] = nil
-        code[j] = nil
-        missed += 1
-        break
-      end
-    end
-    guess.compact!
-    "correct: #{correct}\nwrong position: #{missed}\nwrong: #{guess.length}"
   end
 end
