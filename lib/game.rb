1eeb6857a33330c7828e0378da26d5999220a8c1
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 10:51:53 -0300

    add generate_code method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class Game
-  def initialize; end
+  def initialize
+    @code = []
+    @pegs = %w[red green blue yellow orange purple]
+  end
+
+  def generate_code
+    4.times { code.push(pegs.sample) }
+  end
+
+  private
+
+  attr_accessor :code, :pegs
 end
+
+game = Game.new
+game.generate_code
