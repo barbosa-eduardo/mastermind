0c2d9c6b5d546281a35184f10a6e8300a5bc20e5
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-24 13:25:52 -0300

    refactor Game class methods

--- lib/game.rb
+++ lib/game.rb
@@ -7,22 +7,40 @@ class Game
   end
 
   def play
-  end
-
-  def generate_code
-    4.times { code.push(pegs.sample) }
-    p code
-  end
-
-  def input_guess
-  end
-
-  def generate_feedback(guess)
+    generate_code
+    12.times do
+      guess = input_guess
+      if guess == code
+        end_game('victory')
+        break
+      end
+      puts get_feedback(guess)
+    end
+    end_game('defeat')
   end
 
   private
 
   attr_accessor :code, :pegs
+
+  def generate_code
+    4.times { code.push(pegs.sample) }
+  end
+
+  def input_guess
+  end
+
+  def end_game(state)
+    if state == 'victory'
+      puts 'victory'
+    elsif state == 'defeat'
+      puts 'defeat'
+    end
+    puts "The code was #{code}"
+  end
+
+  def get_feedback(guess)
+  end
 end
 
 game = Game.new
