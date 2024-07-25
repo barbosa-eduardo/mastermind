11e8c848fd0fb2e8f3d53af2c76396d961818781
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-25 20:50:53 -0300

    setup main.rb

--- main.rb
+++ main.rb
@@ -3,4 +3,14 @@
 require_relative 'lib/game'
 
 game = Game.new
-game.play
+
+loop do
+  game.play
+  answer = ''
+  loop do
+    print 'Do you wish to play again? (y/n) '
+    answer = gets.chomp.downcase
+    break if %w[y n].include?(answer)
+  end
+  break if answer == 'n'
+end
