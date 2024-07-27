6ed6b8a338a787256cec99596e2d720ca7ec770a
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-07-26 22:18:37 -0300

    add guess method

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -24,9 +24,8 @@ class CodeBreaker < Code
   end
 
   def generate_guess
-    guess = ''
+    guess = self.guess
 
-    # Save and return guess
     guesses << guess
     guess
   end
@@ -54,5 +53,41 @@ class CodeBreaker < Code
       generate_permutations(pos + 1)
       code.pop
     end
+  end
+
+  def guess
+    return VALID_DIGITS.sample(CODE_LENGTH).join if guesses.empty?
+
+    guess = guesses.last
+    feedback = feedbacks.last
+    correct = feedback[0].to_i
+    included = feedback[2].to_i
+    filter_permutations(guess, correct, included)
+    permutations.sample
+  end
+
+  def filter_permutations(code, correct, included)
+    permutations.each_with_index do |p, i|
+      cor = count_identical_chars(code, p)
+      inc = count_common_chars(code, p)
+      permutations[i] = nil if cor < correct || (inc - cor) < included
+    end
+    permutations.compact!
+  end
+
+  def count_identical_chars(code1, code2)
+    identical_chars = 0
+    code1.chars.each_with_index do |c1, i|
+      identical_chars += 1 if c1 == code2[i]
+    end
+    identical_chars
+  end
+
+  def count_common_chars(code1, code2)
+    common_chars = 0
+    code1.each_char do |c1|
+      common_chars += 1 if code2.include?(c1)
+    end
+    common_chars
   end
 end
